78a20d1796a7354ab31d2b5bc88a1037a29cfdc2
scx1332 <sieciech.czajka@golem.network>
2024-07-07 19:18:57 +0200

    Lower yagna binaries size for action

--- build_self_test_image.sh
+++ build_self_test_image.sh
@@ -1,3 +1,5 @@
 cd golem/ya-self-test-img/dummy
+mkdir -p ../../binaries/plugins/ya-runtime-vm/runtime
 docker build -t ya-self-test-img .
 ../../binaries/gvmkit-build --output self-test.gvmi ya-self-test-img
+cp self-test.gvmi ../../binaries/plugins/ya-runtime-vm/runtime/
