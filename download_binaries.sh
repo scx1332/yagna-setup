7c6817c97e80876793660b5a49a2ebba4e688504
scx1332 <sieciech.czajka@golem.network>
2024-07-07 18:11:10 +0200

    central net router

--- download_binaries.sh
+++ download_binaries.sh
@@ -13,3 +13,7 @@ wget -qO- https://github.com/golemfactory/ya-runtime-vm/releases/download/v0.3.0
 mv ya-runtime-vm-linux-v0.3.0/* plugins/
 rm ya-runtime-vm-linux-v0.3.0 -r
 
+wget -qO- https://github.com/golemfactory/ya-service-bus/releases/download/v0.7.2/ya-sb-router-linux-v0.7.2.tar.gz | tar -xvz
+mv ya-sb-router-linux-v0.7.2/* .
+rm ya-sb-router-linux-v0.7.2 -r
+
