f2d8614e5838bfdd9c32c64ba63ebaa34296e167
scx1332 <sieciech.czajka@golem.network>
2024-07-07 15:21:02 +0200

    fix

--- build_yagna_debug.sh
+++ build_yagna_debug.sh
@@ -1,6 +1,7 @@
 cd golem/yagna
 cargo build --all
+mkdir -p ../binaries/plugins
 cp target/debug/yagna ../binaries/
-cp target/debug/exe-unit ../binaries/
+cp target/debug/exe-unit ../binaries/plugins/
 cp target/debug/ya-provider ../binaries/
 cp target/debug/gftp ../binaries/
--- build_yagna_release.sh
+++ build_yagna_release.sh
@@ -2,6 +2,6 @@ cd golem/yagna
 cargo build --all --release
 mkdir -p ../binaries/plugins
 cp target/release/yagna ../binaries/
-cp target/release/exe-unit ../binaries/
+cp target/release/exe-unit ../binaries/plugins/
 cp target/release/ya-provider ../binaries/
 cp target/release/gftp ../binaries/
