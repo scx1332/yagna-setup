5cb85d39ab1ccc791520ee8dfb91dd0d009a2128
scx1332 <sieciech.czajka@golem.network>
2024-07-07 12:55:52 +0200

    working on scripts

--- run_router.sh
+++ run_router.sh
@@ -1,3 +1,3 @@
-cd golem/ya-service-bus
-cargo run --release -p ya-sb-router --features "bin" -- -l tcp://0.0.0.0:5555
-
+cd golem/ya-sb-router
+cargo build --release -p ya-sb-router --features "bin"
+./target/release/ya-sb-router -l tcp://0.0.0.0:5555
