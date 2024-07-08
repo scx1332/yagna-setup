7ebd74f7d69c13ba97865e03e8d4530b15fba7bc
scx1332 <sieciech.czajka@golem.network>
2024-07-08 13:10:31 +0200

    optimize compression sizes

--- build_runtime_vm_release.sh
+++ build_runtime_vm_release.sh
@@ -1,13 +1,12 @@
 set -x
 cd golem/ya-runtime-vm
-cargo build --release
 mkdir -p ../binaries/plugins/ya-runtime-vm/runtime
-cp target/debug/ya-runtime-vm ../binaries/plugins/ya-runtime-vm/
+(cd runtime/init-container && make)
 cp runtime/conf/ya-runtime-vm.json ../binaries/plugins/
 cp -r runtime/poc/runtime ../binaries/plugins/ya-runtime-vm/
-cp ../ya-self-test-img/dummy/self-test.gvmi ../binaries/plugins/ya-runtime-vm/runtime/
 cp runtime/init-container/initramfs.cpio.gz ../binaries/plugins/ya-runtime-vm/runtime/
 cp runtime/init-container/vmlinuz-virt ../binaries/plugins/ya-runtime-vm/runtime/
-
+cargo build --release
+cp target/debug/ya-runtime-vm ../binaries/plugins/ya-runtime-vm/
 
 
