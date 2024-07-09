f7a30e1feccb5bbfe8c5611e97565de42bc9df6b
scx1332 <sieciech.czajka@golem.network>
2024-07-09 16:50:09 +0200

    testing hybrid

--- prepare_runtime.sh
+++ prepare_runtime.sh
@@ -1,9 +1,9 @@
 set -x
 mkdir -p golem/requestor
-mkdir -p golem/provider
+mkdir -p golem/provider_0
 cp common.env golem/requestor/.env
-cp common.env golem/provider/.env
+cp common.env golem/provider_0/.env
 
-cat provider.env >> golem/provider/.env
+cat provider.env >> golem/provider_0/.env
 cat requestor.env >> golem/requestor/.env
 
