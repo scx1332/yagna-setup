0bd8ada54a6b3834d6345b9514e5f40dfc6e7766
scx1332 <sieciech.czajka@golem.network>
2024-07-09 23:08:31 +0200

    update to pre-rel-v0.16.0-external

--- download_binaries.sh
+++ download_binaries.sh
@@ -1,13 +1,13 @@
 set -x
 mkdir -p golem/downloaded
 cd golem/downloaded
-wget -qO- https://github.com/golemfactory/yagna/releases/download/pre-rel-v0.16.0-external-02/golem-provider-linux-pre-rel-v0.16.0-external-02.tar.gz | tar -xvz
-wget -qO- https://github.com/golemfactory/yagna/releases/download/pre-rel-v0.16.0-external-02/golem-requestor-linux-pre-rel-v0.16.0-external-02.tar.gz | tar -xvz
+wget -qO- https://github.com/golemfactory/yagna/releases/download/pre-rel-v0.16.0-external/golem-provider-linux-pre-rel-v0.16.0-external.tar.gz | tar -xvz
+wget -qO- https://github.com/golemfactory/yagna/releases/download/pre-rel-v0.16.0-external/golem-requestor-linux-pre-rel-v0.16.0-external.tar.gz | tar -xvz
 
-mv golem-provider-linux-pre-rel-v0.16.0-external-02/* .
-mv golem-requestor-linux-pre-rel-v0.16.0-external-02/* .
-rm golem-provider-linux-pre-rel-v0.16.0-external-02 -r
-rm golem-requestor-linux-pre-rel-v0.16.0-external-02 -r
+mv golem-provider-linux-pre-rel-v0.16.0-external/* .
+mv golem-requestor-linux-pre-rel-v0.16.0-external/* .
+rm golem-provider-linux-pre-rel-v0.16.0-external -r
+rm golem-requestor-linux-pre-rel-v0.16.0-external -r
 
 wget -qO- https://github.com/golemfactory/ya-runtime-vm/releases/download/v0.3.0/ya-runtime-vm-linux-v0.3.0.tar.gz | tar -xvz
 mv ya-runtime-vm-linux-v0.3.0/* plugins/
