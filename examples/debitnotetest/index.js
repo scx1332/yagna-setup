13f14681358cb7143fd9c5a200255b54be211086
scx1332 <sieciech.czajka@golem.network>
2024-07-23 15:42:51 +0200

    wip

--- examples/debitnotetest/index.js
+++ examples/debitnotetest/index.js
@@ -17,7 +17,7 @@ debitNotesLost.split(";").forEach((item) => {
     lostDebitNotes.push(parseInt(item));
 })
 console.log(`Lost debit notes used in example: ${lostDebitNotes}`);
-console.log(`Expected script results array: [${" ".join(expectedScriptResult)}]`);
+console.log(`Expected script results array: [${" ".join(expectedScriptResults)}]`);
 
 const myDebitNoteFilter = async (debitNote, _context) => {
     let debitNo = debitNotesReceived.push(debitNote.id);
