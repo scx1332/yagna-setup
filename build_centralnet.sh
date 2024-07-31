ac9b06b8be8e1b5c1dc804206742c3af2f66e409
jalas167 <42438166+jalas167@users.noreply.github.com>
2024-07-31 12:39:46 +0200

    Update build_centralnet.sh

--- build_centralnet.sh
+++ build_centralnet.sh
@@ -2,4 +2,4 @@
 set -e
 cd golem/ya-service-bus
 cargo build --release --target x86_64-unknown-linux-musl -p ya-sb-router --features "bin"
-cp target/release/ya-sb-router ../binaries/
+cp target/x86_64-unknown-linux-musl/release/ya-sb-router ../binaries/
